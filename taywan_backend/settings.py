eb64866015eae775975ba68bdb53f9e34726e8fb
amenabe22 <bdere12345@gmail.com>
2021-04-17 03:14:29 +0300

    new changes

--- taywan_backend/settings.py
+++ taywan_backend/settings.py
@@ -54,6 +54,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
 ]
 
 ROOT_URLCONF = 'taywan_backend.urls'
